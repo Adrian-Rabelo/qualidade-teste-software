9d6523a37daa6e3a54c197b9b441c83ef349a953
Adrian Rabelo <adrianrabeloe10@gmail.com>
2021-09-10 20:04:50 -0400

    criando método para obter data

--- src/main/java/Util/DataUtil.java
+++ src/main/java/Util/DataUtil.java
@@ -15,4 +15,13 @@ public class DataUtil {
 		
 	}
 	
+	public Date obterData(int dia, int mes, int ano) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, dia);
+		calendar.set(Calendar.MONTH, mes - 1);
+		calendar.set(Calendar.YEAR, ano);
+		
+		return calendar.getTime();
+	}
+	
 }
