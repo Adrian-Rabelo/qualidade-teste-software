b3f3a3930e2292dc55f1331b5b6b4cc393677a5a
Adrian Rabelo <adrianrabeloe10@gmail.com>
2021-09-11 14:17:07 -0400

    migrando método de teste para uma classe específica que realizará os testes

--- src/main/java/qts/locadora/service/LocacaoService.java
+++ src/main/java/qts/locadora/service/LocacaoService.java
@@ -28,23 +28,4 @@ public class LocacaoService {
 		return locacao;
 	}
 	
-	@Test
-	public void teste() {
-
-		// Cenário
-		Cliente cliente = new Cliente("Ádrian");
-		Jogo jogo = new Jogo("God of War",40.66, 5);
-		
-		// Ação
-		LocacaoService locacaoService = new LocacaoService();
-		Locacao locacao = locacaoService.alugarJogo(cliente, jogo);
-		
-		// Verificação
-		Assert.assertTrue(("Erro no jogo seleionado para locação"),locacao.getJogo().getNome().equals(jogo.getNome()));
-		Assert.assertTrue(("Erro nos dados do cliente"), locacao.getCliente().getNome().equals(cliente.getNome()));
-		Assert.assertTrue(("Erro no valor da locação"), (locacao.getValor()==jogo.getPreco()));
-		Assert.assertTrue(("Erro na data ad locação"), (new DataUtil().verificaDatasIguais(locacao.getRetirada(), new Date())));
-		
-	}
-
 }
